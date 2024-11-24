24c444bca690597f08e2d062b42851bec582aa4b
leonardo-0202 <leonardodenovellis2@gmail.com>
2024-11-24 11:09:12 +0100

    Created struct InputData, started implementation of Reader class

--- src/Reader.cpp
+++ src/Reader.cpp
@@ -1,4 +1,37 @@
 #include <string>
+#include <fstream>
+#include <iostream>
+#include <sstream>
+#include <Eigen/Dense>
 #include "Reader.h"
+#include "InputData.h"
 
-Reader::Reader(std::string file_name) {}
+Reader::Reader(std::string file_name, std::string file_type,
+               long num_rows, long num_cols, std::string method,
+               long num_iters, double toll)
+{
+
+}
+
+Eigen::MatrixXd Reader::readMatrixCSV(std::string file_name, long num_rows,
+    long num_cols)
+{
+    Eigen::MatrixXd A;
+    std::ifstream file(file_name);
+    if (!file.is_open()) {
+        // throw error
+    }
+
+    std::string line;
+    int row = 0;
+    while (getline(file, line) && row < num_rows) {
+        std::stringstream ss(line);
+        std::string cell;
+        int col = 0;
+        while (getline(ss, cell, ',') && col < num_cols) {
+            A[row][col] = cell;
+            col++;
+        }
+        row++;
+    }
+}
--- src/main.cc
+++ src/main.cc
@@ -1,22 +1,28 @@
-#include <iostream>
-
-#include <Eigen/Dense>
-#include <Eigen/Sparse>
+#include "pcsc.hh"
 #include "Reader.h"
 #include "Solver.h"
-#include "Output.h"
+#include <Eigen/Dense>
+#include <iostream>
 
-int main(int argc, char **argv) 
+int main(int argc, char **argv)
 {
-    // Read data and get matrix
-    Reader data_reader(argv[0]);
-    Eigen::VectorXd input_matrix = data_reader.readData();
-    
-    // Solver
-    Solver eigen_solver(input_matrix);
-    Eigen::VectorXd eigens = solver.PowerMethod();
-    // Other methods
+    std::string file_name = "./test";
+    std::string file_type = "csv";
+    std::string method = "Power";
+    long num_rows = 100;
+    long num_cols = 100;
+    long num_iters = 100;
+    double toll = 10e-5;
 
-    // Output 
-    Output output(eigens); // other args maybe from input
+    Reader data_reader = Reader(
+        file_name, file_type, num_rows, num_cols,
+        method, num_iters, toll
+        );
+
+    // Solver solver(data_reader.getInputData());
+    // Output output = Solver.solve();
 }
+
+// Reader should take all users input, and save it to a custom
+// data class, which then gets passed to a solver, which then
+// creates an output which extracts solution to a CSV
