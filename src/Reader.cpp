3425354e0cb9ed0ff0e24ee013cfab9bb2ba024f
leonardo-0202 <leonardodenovellis2@gmail.com>
2024-11-26 17:39:06 +0100

    Implemented readMatrixCSV function (still need to implement error handling)

--- src/Reader.cpp
+++ src/Reader.cpp
@@ -3,35 +3,44 @@
 #include <iostream>
 #include <sstream>
 #include <Eigen/Dense>
-#include "Reader.h"
-#include "InputData.h"
 
-Reader::Reader(std::string file_name, std::string file_type,
+/*#include "Reader.h"
+#include "InputData.h"*/
+
+/*Reader::Reader(std::string file_name, std::string file_type,
                long num_rows, long num_cols, std::string method,
                long num_iters, double toll)
 {
 
 }
+*/
 
-Eigen::MatrixXd Reader::readMatrixCSV(std::string file_name, long num_rows,
+Eigen::MatrixXd readMatrixCSV(std::string const& file_name, long const num_rows,
     long num_cols)
 {
-    Eigen::MatrixXd A;
+    Eigen::MatrixXd A(num_rows, num_cols);
     std::ifstream file(file_name);
     if (!file.is_open()) {
-        // throw error
+        std::cerr << "Error opening file: " << file_name << std::endl;
+        return Eigen::MatrixXd(); // Return an empty matrix for now,
+        // can look into throwing exception
     }
-
     std::string line;
-    int row = 0;
-    while (getline(file, line) && row < num_rows) {
+    long row = 0;
+    while (std::getline(file, line) && row < num_rows) {
         std::stringstream ss(line);
         std::string cell;
-        int col = 0;
+        long col = 0;
         while (getline(ss, cell, ',') && col < num_cols) {
-            A[row][col] = cell;
+            try {
+                A(row, col) = std::stod(cell);
+            } catch (const std::invalid_argument& e) {
+                std::cerr << "Invalid number at row " << row << ", column " << col << std::endl;
+                A(row, col) = -10e9;
+            }
             col++;
         }
         row++;
     }
+    return A;
 }
