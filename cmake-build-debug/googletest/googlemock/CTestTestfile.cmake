e626d77faf4809bd91bca7472072c8076099e0e1
leonardo-0202 <leonardodenovellis2@gmail.com>
2024-11-27 19:09:59 +0100

    Created reader subclasses. Implemented read method for FileReader.

---
+++ cmake-build-debug/googletest/googlemock/CTestTestfile.cmake
@@ -0,0 +1,7 @@
+# CMake generated Testfile for 
+# Source directory: C:/Users/giova/Documents/GitHub/pcsc-project-template/googletest/googlemock
+# Build directory: C:/Users/giova/Documents/GitHub/pcsc-project-template/cmake-build-debug/googletest/googlemock
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs("../googletest")
