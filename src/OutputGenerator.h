9925baa13b4d4c42b3459aced74bf486feb99c9c
leonardo-0202 <leonardodenovellis2@gmail.com>
2024-12-06 13:12:22 +0100

    Added writeToCSV method

--- src/OutputGenerator.h
+++ src/OutputGenerator.h
@@ -9,6 +9,9 @@ protected:
     OutputData out;
 public:
     OutputGenerator(OutputData output);
+    std::string writeToHTML(const Eigen::VectorXcd & vec);
+    void writeToCSV(const Eigen::VectorXcd & vec, std::string out_path);
+    std::string warningHTML(int err);
     void saveOutput();
 };
 
