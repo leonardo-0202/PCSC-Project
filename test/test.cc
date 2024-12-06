48fae8f7330da5d976324cff738c298c10ae48d9
leonardo-0202 <leonardodenovellis2@gmail.com>
2024-12-06 16:52:39 +0100

    Adding testing blueprints

--- test/test.cc
+++ test/test.cc
@@ -1,4 +1,22 @@
 #include <cmath>
 #include <gtest/gtest.h>
+#include "utils.h"
 
-TEST(sqrt, integer) { EXPECT_EQ(2, std::sqrt(4)); }
+TEST(createReader, Files) {
+    Eigen::MatrixXcd correct_mat = Eigen::MatrixXcd::Zero(4, 4);
+    EXPECT_EQ( createReader("../config.json")->getInputData().input_matrix,
+        correct_mat );
+}
+
+TEST(createReader, Picture) {
+    Eigen::MatrixXcd correct_mat = Eigen::MatrixXcd::Zero(4, 4);
+    EXPECT_EQ( createReader("../config.json")->getInputData().input_matrix,
+        correct_mat );
+}
+
+TEST(createReader, Function) {
+    Eigen::MatrixXcd correct_mat = Eigen::MatrixXcd::Zero(4, 4);
+    EXPECT_EQ( createReader("../config.json")->getInputData().input_matrix,
+        correct_mat );
+}
+
