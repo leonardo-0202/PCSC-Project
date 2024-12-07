472a44c39e83c84faa8bd2c9aea2f389155ba5fa
Simon Anton <scanton@Simon-Legion>
2024-12-07 11:23:34 +0100

    Doxygen comments for Solver.h

--- src/Solver.h
+++ src/Solver.h
@@ -5,44 +5,108 @@
 #include "OutputData.h"
 #include <Eigen/Dense>
 
+/** @file Solver.h
+ * @brief Header file for all Solver based classes.
+ */
+
 class Solver
 {
     protected:
-        int n;
-        int num_iters;
-        double tol;
-        Eigen::MatrixXcd A;
-        OutputData output;
-        std::string method_name;
+        int n; /// Dimension of input matrix
+        int num_iters; /// Maximum number of iterations for method
+        double tol; /// Tolerance for measuring convergence
+        Eigen::MatrixXcd A; /// Input matrix
+        OutputData output; /// Struct for data gathered during solving
+        std::string method_name; /// Name of method used
     public:
+        /**
+         * @brief Abstract Solver constructor
+         * @param input The input data of the problem to initialize protected attributes.
+         */
         Solver(InputData input);
+        /**
+         * @brief Method to get OutputData struct
+         * @return OutputData struct
+         */
         OutputData getOutput();
+        /**
+         * @brief Pure virtual function for running a solver
+         * @return void: Alter OutputData object.
+         */
         virtual void solve() = 0;
 };
 
+/**
+ * @class PowerBasedSolver
+ * @brief Derived Solver class. Abstract class for power based solvers. 
+ * 
+ * The eigenvec_approx method must be overriden by daughter classes.
+ */
 class PowerBasedSolver : public Solver
 {
     protected:
+        /// Shifted matrix given by: @f$ A - \mu I @f$ 
         Eigen::MatrixXcd shifted_matrix;
+        /**
+         * @brief Pure virtual function for eigenvector approximations. 
+         * @param b Current approximation of the eigenvector. Passed to the method as a const reference to 'Eigen::VectorXcd'.
+         * @return 'Eigen::VectorXcd' representing a step in approximating the eigenvector before normalization.
+         */
         virtual Eigen::VectorXcd eigenvec_approx(Eigen::VectorXcd const& b) = 0;
     public:
+        /**
+        * @brief Base constructor for any PowerBased derived class.
+        * @param input The input data of the problem contained in InputData struct.
+        */
         PowerBasedSolver(InputData input); 
+        /**
+         * @brief Overriden solve method from Solver class. 
+         * Generalized to work for shift, power, and inverse methods. 
+         * @return void: Alter OutputData object from Solver class.
+         */
         void solve() override;
 };
 
+/**
+ * @class PowerSolver
+ * @brief Derived PowerBasedSolver class. Implements specifics of power method. 
+ * 
+ * The eigenvec_approx method is implemented. 
+ */
 class PowerSolver : public PowerBasedSolver
 {
     public:
+        /**
+         * @brief PowerSolver constructor
+         * @param input The input data of the problem contained in InputData struct.
+         */
         PowerSolver(InputData input);
+        /**
+         * @brief Power method implementation of eigenvector approximation at each iteration. 
+         */
         Eigen::VectorXcd eigenvec_approx(Eigen::VectorXcd const& b) override;
 };
 
+/**
+ * @class InverseSolver
+ * @brief Derived PowerBasedSolver class. Implements specifics of inverse power method. 
+ * 
+ * The eigenvec_approx method is implemented. 
+ */
 class InverseSolver : public PowerBasedSolver
 {
     protected:
+        /// Matrix decomposition
         Eigen::CompleteOrthogonalDecomposition<Eigen::MatrixXcd> decomp;
     public:
+        /**
+         * @brief InverseSolver constructor
+         * @param input The input data of the problem contained in InputData struct.
+         */
         InverseSolver(InputData input);
+        /**
+         * @brief Inverse power method implementation of eigenvector approximation at each iteration.
+         */
         Eigen::VectorXcd eigenvec_approx(Eigen::VectorXcd const& b) override;
 };
 
@@ -54,19 +118,15 @@ class QRSolver : public Solver
 {
     public:
         /**
-        * @brief Consturcts a QRSolver object.
-        * @param input The input data of the problem.
+        * @brief Constructs a QRSolver object.
+        * @param input The input data of the problem contained in InputData struct.
         */
         QRSolver(InputData input);
         /**
-        * @brief Computes the Q matrix in the QR decomposition of the input matrix.
-        * @param A The matrix to find the decomposition of.
+        * @brief Overriden solve method from Solver class. 
+        * Solves for the eigenvalues using the QR method.
         */
-        Eigen::MatrixXcd QRDecompQ(Eigen::MatrixXcd A);
-        /**
-        * @brief Solves for the eigenvalues using the QR method.
-        */
-        void solve();
+        void solve() override;
 };
 
 #endif //SOLVER_H
